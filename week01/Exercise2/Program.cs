26e80dd31480f8ebf1c70a15779b837e306dfc59
buy-st <stalatala@byupathway.edu>
2025-05-09 12:03:17 +0200

    finished 2nd exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What's your grade percentage? ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        // Console.WriteLine($"Your grade is {letter}");
+
+        //Lets Assume you must at least have 70 percent to pass
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations!You have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Your grade was not satisfactory,Please try again!");
+        }
+     
+       //Add sign to your letter
+        int lastDigit = grade % 10;
+        string sign ="";
+
+        if (lastDigit >= 7 && grade < 97)
+        {
+            sign = "+";
+        }
+
+        else if (lastDigit < 3)
+        {
+         sign = "-";   
+        }
+            
+       
+    Console.WriteLine($"your grade is {letter}{sign}");
     }
 }
