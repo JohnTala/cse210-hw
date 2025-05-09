0f4ebebda3fbca5b6aa6cfdd0c7ee9e2d463912e
buy-st <stalatala@byupathway.edu>
2025-05-09 16:01:24 +0200

    finished 3rd exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        bool question = true;
+        int guess;
+        int guesses;
+        string answer;
+        int magicNumber;
+        while (question)
+        {
+            //  int magicNumber = -1;//core 2
+            answer = "";
+            guess = 0;
+            guesses = 0;
+
+            Console.WriteLine("what's the magic number? ");
+            magicNumber = randomGenerator.Next(1, 10);
+            // magicNumber = Convert.ToInt32(Console.ReadLine());//core 2
+
+
+            while (guess != magicNumber)
+            {
+
+                Console.Write("what's your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Your guess is {guess}");
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Your guess is Higher");
+
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Your guess is Lower");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                guesses += 1;
+            }
+            Console.WriteLine($"The number of attempt of guesses is {guesses}!");
+            Console.WriteLine();
+            Console.Write("Would like to play again? (Y/N)? ");
+            answer = Console.ReadLine();
+            answer = answer.ToUpper();
+            if (answer == "Y")
+            {
+                question = true;
+            }
+            else
+            {
+                question = false;
+            }
+        }
+        Console.WriteLine("Thank you for playing! Good bye! ");
+        
+
     }
 }
